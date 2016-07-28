3e8ab8410e4ca6bf1fb2a9f7266a43a42f7149ab
PHAMBAQUY <pbquy96@gmail.com>
2016-07-28 21:50:08 +0700

    09:50 PM, 28-07-2016

--- Recommend.py
+++ Recommend.py
@@ -1,5 +1,11 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.client import GoogleCredentials
+from apiclient.http import MediaFileUpload
 from Predict_Null_Value import predict
 from math import sqrt
+import time
+
 
 def pearson(rating1, rating2):
     sum_xy = 0
@@ -23,39 +29,41 @@ def pearson(rating1, rating2):
     # now compute denominator
     denominator = sqrt(sum_x2 - pow(sum_x, 2) / n) * \
                   sqrt(sum_y2 - pow(sum_y, 2) / n)
+
     if denominator == 0:
         return 0
     else:
         return (sum_xy - (sum_x * sum_y) / n) / denominator
 
 
-def computeNearestNeighbor(data, id_sku):
+def computeNearestNeighbor(data, username):
     """creates a sorted list of users based on their distance
     to username"""
     distances = []
-    for instance in data.keys():
-        if instance != id_sku:
-            distance = pearson(data[id_sku],
+    for instance in data:
+        if instance != username:
+            distance = pearson(data[username],
                                data[instance])
-            distances.append((instance, distance))
+            if abs(distance) != 0:
+                distances.append((instance, distance))
     # sort based on distance -- closest first
     distances.sort(key=lambda artistTuple: artistTuple[1],
                    reverse=True)
     return distances
 
 
-def recommend(data, id, sku, k):
+def recommend(data, user, k):
     """Give list of recommendations"""
     recommendations = {}
-    id_sku = (id, sku)
-
     # first get list of users  ordered by nearness
-    nearest = computeNearestNeighbor(data, id_sku)
-    print(nearest)
+    nearest = computeNearestNeighbor(data, user)
+    if len(nearest) < k:
+        k = len(nearest)
     #
     # now get the ratings for the user
     #
-    userRatings = data[id_sku]
+    userRatings = data[user]
+    # print(nearest)
     #
     # determine the total distance
     totalDistance = 0.0
@@ -63,6 +71,8 @@ def recommend(data, id, sku, k):
         totalDistance += nearest[i][1]
     # now iterate through the k nearest neighbors
     # accumulating their ratings
+    if totalDistance == 0:
+        return []
     for i in range(k):
         # compute slice of pie
         weight = nearest[i][1] / totalDistance
@@ -81,15 +91,151 @@ def recommend(data, id, sku, k):
                     recommendations[artist] = recommendations[artist] + \
                                               neighborRatings[artist] * \
                                               weight
-    # now make list from dictionary and only get the first n items
+    # now make list from dictionary and only get the first k items
     recommendations = list(recommendations.items())[:k]
-    recommendations = [(k__, v)
-                       for (k__, v) in recommendations]
+    recommendations = [(u, v)
+                       for (u, v) in recommendations]
     # finally sort and return
     recommendations.sort(key=lambda artistTuple: artistTuple[1],
                          reverse=True)
     return recommendations
 
-data = predict()
-res = recommend(data, 'c05', 'a6', 5)
-print(res)
+
+def createData():
+    data = predict()
+    res = {}
+    # This factor is just random. It will change later.
+    factor = {'sales': 1.6, 'views': 1.4, 'carts': 2, 'sales_effective_rate': 1, 'rating': 1.2, 'comments': 1}
+    for (item, rating) in data.items():
+        tmp = 0
+        for (key, num) in factor.items():
+            tmp += rating[key] * num
+        res[item] = tmp
+
+    data = {}
+    for (item1, rating1) in res.items():
+        if item1[0] not in data:
+            data.setdefault(item1[0], {})
+            data[item1[0]][item1[1]] = rating1
+        for (item2, rating2) in res.items():
+            if item1[0] == item2[0] and item2[1] not in data[item2[0]]:
+                data[item2[0]][item2[1]] = rating2
+    return data
+
+
+def createTable(service):
+    project_id = "598330041668"
+    dataset_id = 'recommendation_001'
+    table_id = 'product_recommended'
+
+    tables = service.tables()
+    table_ref = {'tableId': table_id,
+                 'datasetId': dataset_id,
+                 'projectId': project_id}
+    # [Delete table]
+    tables.delete(**table_ref).execute()
+
+    # [START create new table]
+    request_body = {
+        "schema": {
+            "fields": [
+                {
+                    "mode": "NULLABLE",
+                    "type": "STRING",
+                    "name": "customer_id",
+                },
+                {
+                    "mode": "NULLABLE",
+                    "type": "STRING",
+                    "name": "sku",
+                },
+                {
+                    "mode": "NULLABLE",
+                    "type": "FLOAT",
+                    "name": "distance",
+                },
+
+            ],
+        },
+        "tableReference": {
+            "projectId": project_id,
+            "tableId": table_id,
+            "datasetId": dataset_id
+        }
+    }
+
+    response = tables.insert(projectId=project_id,
+                             datasetId=dataset_id,
+                             body=request_body).execute()
+    # [END create new table]
+    # print out the response
+
+
+def insertValues(service):
+    project_id = "598330041668"
+    dataset_id = "recommendation_001"
+    table_id = "product_recommended"
+
+    # Load configuration with the destination specified.
+    load = {'destinationTable': {
+        'projectId': project_id,
+        'datasetId': dataset_id,
+        'tableId': table_id
+    }, 'schema': {
+        'fields': [
+            {'name': 'customer_id', 'type': 'STRING'},
+            {'name': 'sku', 'type': 'STRING'},
+            {'name': 'distance', 'type': 'FLOAT'},
+        ]
+    }, 'sourceFormat': 'NEWLINE_DELIMITED_JSON'}
+
+    # This tells it to perform a resumable upload of a local file
+    # called 'foo.json'
+    upload = MediaFileUpload('result.json',
+                             mimetype='application/octet-stream',
+                             # This enables resumable uploads.
+                             resumable=True)
+
+    start = time.time()
+    job_id = 'job_%d' % start
+    # Create the job.
+    result = service.jobs().insert(
+        projectId=project_id,
+        body={
+            'jobReference': {
+                'jobId': job_id
+            },
+            'configuration': {
+                'load': load
+            }
+        },
+        media_body=upload).execute()
+
+    # [END run_query]
+
+
+def result():
+    data = createData()
+    k = 10
+    f = open('result.json', 'w')
+
+    for user in data.keys():
+        tmp = recommend(data, user, k)
+        if tmp != []:
+            res = {}
+            for (sku, distance) in tmp:
+                res['customer_id'] = user
+                res['sku'] = sku
+                res['distance'] = distance
+                f.write(str(res))
+                f.write('\n')
+    f.close()
+
+    credentials = GoogleCredentials.get_application_default()
+    bigquery_service = build('bigquery', 'v2', credentials=credentials)
+
+    #createTable(bigquery_service)
+    insertValues(bigquery_service)
+
+
+result()
